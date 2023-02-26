0d1519266b19a46fa283fbf61c016b2020c9e5fe
cokorie <chijioke.okorie@gmail.com>
2023-02-26 06:15:07 -0600

    Update Player Speed to Original

--- Assets/MovingBlock.cs
+++ Assets/MovingBlock.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovingBlock : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 30f;
+    [SerializeField] float moveSpeed = 3f;
 
     float startingYPosition;
     public static int Score;
