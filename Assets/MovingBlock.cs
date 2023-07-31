1bb2f0a287225b0a97e843d4a95e75a46f8135c9
cokorie <chijioke.okorie@gmail.com>
2023-07-30 21:29:50 -0500

    increase moveSpeed value by 4 times original

--- Assets/MovingBlock.cs
+++ Assets/MovingBlock.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovingBlock : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float moveSpeed = 12f;
 
     float startingYPosition;
     public static int Score;
