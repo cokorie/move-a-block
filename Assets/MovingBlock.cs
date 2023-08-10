6244773a5938f21553b4451b01916efcdacdfbf6
cokorie <chijioke.okorie@gmail.com>
2023-08-10 15:26:00 -0500

    revert moveSpeed to original

--- Assets/MovingBlock.cs
+++ Assets/MovingBlock.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovingBlock : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 9f;
+    [SerializeField] float moveSpeed = 3f;
 
     float startingYPosition;
     public static int Score;
